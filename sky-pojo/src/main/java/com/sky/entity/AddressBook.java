32d8821fd47bca851ac9c500a086d0b0f175341d
wryyyy4444 <34270450+wryyyy4444@users.noreply.github.com>
2024-07-29 07:06:25 +0800

    docs: Add swagger for entity.

--- sky-pojo/src/main/java/com/sky/entity/AddressBook.java
+++ sky-pojo/src/main/java/com/sky/entity/AddressBook.java
@@ -1,5 +1,7 @@
 package com.sky.entity;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -14,48 +16,50 @@ import java.io.Serializable;
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
+@ApiModel(description = "地址簿实体类")
 public class AddressBook implements Serializable {
 
-    private static final long serialVersionUID = 1L;
+	private static final long serialVersionUID = 1L;
 
-    private Long id;
+	@ApiModelProperty(value = "地址簿ID", example = "1")
+	private Long id;
 
-    //用户id
-    private Long userId;
+	@ApiModelProperty(value = "用户ID", example = "1")
+	private Long userId;
 
-    //收货人
-    private String consignee;
+	@ApiModelProperty(value = "收货人", example = "张三")
+	private String consignee;
 
-    //手机号
-    private String phone;
+	@ApiModelProperty(value = "手机号", example = "13800138000")
+	private String phone;
 
-    //性别 0 女 1 男
-    private String sex;
+	@ApiModelProperty(value = "性别 0 女 1 男", example = "1", allowableValues = "0, 1")
+	private String sex;
 
-    //省级区划编号
-    private String provinceCode;
+	@ApiModelProperty(value = "省级区划编号", example = "110000")
+	private String provinceCode;
 
-    //省级名称
-    private String provinceName;
+	@ApiModelProperty(value = "省级名称", example = "北京市")
+	private String provinceName;
 
-    //市级区划编号
-    private String cityCode;
+	@ApiModelProperty(value = "市级区划编号", example = "110100")
+	private String cityCode;
 
-    //市级名称
-    private String cityName;
+	@ApiModelProperty(value = "市级名称", example = "北京市")
+	private String cityName;
 
-    //区级区划编号
-    private String districtCode;
+	@ApiModelProperty(value = "区级区划编号", example = "110101")
+	private String districtCode;
 
-    //区级名称
-    private String districtName;
+	@ApiModelProperty(value = "区级名称", example = "东城区")
+	private String districtName;
 
-    //详细地址
-    private String detail;
+	@ApiModelProperty(value = "详细地址", example = "东华门街道")
+	private String detail;
 
-    //标签
-    private String label;
+	@ApiModelProperty(value = "标签", example = "家")
+	private String label;
 
-    //是否默认 0否 1是
-    private Integer isDefault;
+	@ApiModelProperty(value = "是否默认 0否 1是", example = "1", allowableValues = "0, 1")
+	private Integer isDefault;
 }
