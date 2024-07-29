91512f9206bec25c811e8541562199c1ef01d177
wryyyy4444 <34270450+wryyyy4444@users.noreply.github.com>
2024-07-30 00:24:51 +0800

    docs: test.

--- sky-server/src/main/java/com/sky/SkyApplication.java
+++ sky-server/src/main/java/com/sky/SkyApplication.java
@@ -1,16 +1,42 @@
 package com.sky;
 
+import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.core.env.Environment;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
+import java.net.InetAddress;
+
 @SpringBootApplication
-@EnableTransactionManagement //开启注解方式的事务管理
+@EnableTransactionManagement // 开启注解方式的事务管理
 @Slf4j
 public class SkyApplication {
+	private static final Logger logger = LoggerFactory.getLogger(SkyApplication.class);
+
+	@SneakyThrows
 	public static void main(String[] args) {
-		SpringApplication.run(SkyApplication.class, args);
-		log.info("server started");
+		SpringApplication app = new SpringApplication(SkyApplication.class);
+		ConfigurableApplicationContext application = app.run(args);
+		Environment env = application.getEnvironment();
+		logger.info("\n----------------------------------------------------------\n\t" +
+						"Application '{}' is running! Access URLs:\n\t" +
+						"Local: \t\thttp://localhost:{}\n\t" +
+						"Local_Doc: \t\thttp://localhost:{}/doc.html\n\t" +
+						"External: \thttp://{}:{}\n\t" +
+						"External_Doc: \thttp://{}:{}/doc.html\n" +
+						"----------------------------------------------------------",
+				env.getProperty("spring.application.name"),
+				env.getProperty("server.port"),
+				env.getProperty("server.port"),
+				InetAddress.getLocalHost().getHostAddress(),
+				env.getProperty("server.port"),
+				InetAddress.getLocalHost().getHostAddress(),
+				env.getProperty("server.port"));
 	}
+
 }
