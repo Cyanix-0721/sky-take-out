91394aa86cffa2779da51d68c90f1f5143447eab
wryyyy4444 <34270450+wryyyy4444@users.noreply.github.com>
2024-08-04 16:18:23 +0800

    chore: 更换手动新建Logger对象为@Slf4j注解.

--- sky-server/src/main/java/com/sky/handler/MyMetaObjectHandler.java
+++ sky-server/src/main/java/com/sky/handler/MyMetaObjectHandler.java
@@ -3,20 +3,19 @@ package com.sky.handler;
 import com.baomidou.mybatisplus.core.handlers.MetaObjectHandler;
 import com.sky.constant.AutoFillConstant;
 import com.sky.context.BaseContext;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.ibatis.reflection.MetaObject;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 import java.time.LocalDateTime;
 
+@Slf4j
 @Component
 public class MyMetaObjectHandler implements MetaObjectHandler {
-	private static final Logger logger = LoggerFactory.getLogger(MyMetaObjectHandler.class);
 
 	@Override
 	public void insertFill(MetaObject metaObject) {
-		logger.info("Start insert fill...");
+		log.info("Start insert fill...");
 		this.setFieldValByName(AutoFillConstant.CREATE_TIME, LocalDateTime.now(), metaObject);
 		this.setFieldValByName(AutoFillConstant.UPDATE_TIME, LocalDateTime.now(), metaObject);
 		this.setFieldValByName(AutoFillConstant.CREATE_USER, BaseContext.getCurrentId(), metaObject);
@@ -26,7 +25,7 @@ public class MyMetaObjectHandler implements MetaObjectHandler {
 
 	@Override
 	public void updateFill(MetaObject metaObject) {
-		logger.info("Start update fill...");
+		log.info("Start update fill...");
 		this.setFieldValByName(AutoFillConstant.UPDATE_TIME, LocalDateTime.now(), metaObject);
 		this.setFieldValByName(AutoFillConstant.UPDATE_USER, BaseContext.getCurrentId(), metaObject);
 		this.setFieldValByName(AutoFillConstant.USER_ID, BaseContext.getCurrentId(), metaObject);
